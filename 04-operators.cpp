0eb52a2d55bcbe79cdeed64a7a719e04da413ab9
KentBersabalPersonal <denzelbersabal44@gmail.com>
2026-08-15 12:49:57 +0800

    Implement basic arithmetic operations in C++
    This program performs basic arithmetic operations on two integers and prints the results.

---
+++ 04-operators.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int a = 10;
+  int b = 3;
+
+  cout << "\nSum: " << a + b << endl;
+
+  cout << "Difference: " << a - b << endl;
+
+  cout << "Product: " << a * b << endl;
+
+  cout << "Quotient: " << a / b << endl;
+
+  cout << "Remainder: " << a % b << endl;
+
+  return 0;
+}
+
+// OUTPUT IS: 
+
+// Sum: 1
+// Difference: 7
+// Product: 30
+// Quotient: 3
+// Remainder: 1
+
+/*
+ * MAIN IDEA:
+ * This program declares two integer variables, a and b, then performs
+ * basic arithmetic operations on them and prints each result.
+*/
