ea419dec9c3d52693072df8778d97217efca2548
KentBersabalPersonal <denzelbersabal44@gmail.com>
2026-08-15 11:37:43 +0800

    Input and output in C++
    This program prompts the user for their name and greets them.

---
+++ 03-Input-cin.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string name;
+    cout << "\nEnter your name: ";
+  
+    cin >> name;
+    cout << "\nHello, " << name << "!" << endl;
+    
+  return 0;
+}
+
+//  THE OUTPUT IS - HELLO, ENTERED NAME!
+
+/*
+ * MAIN IDEA:
+ * This program asks the user to type their name (input), stores it in the
+ * "name" variable, then greets them using that input.
+ *
+ * cout << ...  -> displays text/prompts on the screen (output)
+ * cin >> name; -> waits for the user to type something and saves it into "name" (input)
+ */
